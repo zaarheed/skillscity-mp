fdd6d6421f215b30cc8030776ffba3c2217c1af1
Zahid <zahid@zmdev.com>
2023-02-14 11:13:02 +0000

    re-useable Input component

---
+++ components/forms/input/index.js
@@ -0,0 +1,14 @@
+export default function Input(props) {
+    const handleChange = (event) => {
+        const value = event.target.value;
+        props.onChange(value);
+    }
+
+    return (
+        <input
+            {...props}
+            className="bg-gray-200 py-3 px-6 w-96 rounded"
+            onChange={handleChange}
+        />
+    )
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+import Input from "@/components/forms/input";
 import Content from "@/components/shared/content";
 import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
@@ -8,16 +9,6 @@ export default function Contact() {
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const [showSuccess, setShowSuccess] = useState(false);
-
-    const handleChangeName = (event) => {
-        const name = event.target.value;
-        setName(name);
-    }
-
-    const handleChangeEmail = (event) => {
-        const email = event.target.value;
-        setEmail(email);
-    }
 
     const handleChangeMessage = (event) => {
         const message = event.target.value;
@@ -40,17 +31,16 @@ export default function Contact() {
 
             <Content>
                 <form className="flex flex-col space-y-3">
-                    <input
+                    <Input
                         placeholder="Name"
-                        className="bg-gray-200 py-3 px-6 w-96 rounded"
                         value={name}
-                        onChange={handleChangeName}
+                        onChange={(value) => setName(value)}
+                        disabled={true}
                     />
-                    <input
+                    <Input
                         placeholder="Email"
-                        className="bg-gray-200 py-3 px-6 w-96 rounded"
                         value={email}
-                        onChange={handleChangeEmail}
+                        onChange={(value) => setEmail(value)}
                     />
                     <textarea
                         placeholder="Message"
