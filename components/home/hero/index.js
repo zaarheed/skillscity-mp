7d5f1b8304b6042279fa56e62fcc9777f195bdd5
Zahid <zahid@zmdev.com>
2023-02-15 11:42:46 +0000

    mobile responsive hero on homepage

--- components/home/hero/index.js
+++ components/home/hero/index.js
@@ -1,12 +1,12 @@
 export default function Hero({ imgUrl, title, subtitle }) {
     return (
         <div className="w-full max-w-6xl mx-auto">
-            <div className="w-full flex flex-row space-x-2 py-4">
-                <div className="w-2/12">
+            <div className="w-full flex flex-col items-center py-4 space-y-4 md:flex-row md:space-x-4">
+                <div className="w-48">
                     <img src={imgUrl} className="rounded-lg" />
                 </div>
-                <div className="w-10/12">
-                    <h1 className="text-4xl font-bold">{title}</h1>
+                <div className="w-full text-center md:text-left">
+                    <h1 className="text-6xl font-bold">{title}</h1>
                     <p className="text-xl text-gray-700">{subtitle}</p>
                 </div>
             </div>
