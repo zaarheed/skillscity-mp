7247461ad5cdd4b7e476f71f4ee5146244cc9eb9
Zahid <zahid@zmdev.com>
2023-02-16 11:55:26 +0000

    px-4 on content component

--- components/shared/content/index.js
+++ components/shared/content/index.js
@@ -1,6 +1,6 @@
 export default function Content({ children }) {
     return (
-        <div className="w-full max-w-6xl mx-auto mt-5">
+        <div className="w-full max-w-6xl mx-auto mt-5 px-4">
             {children}
         </div>
     );
