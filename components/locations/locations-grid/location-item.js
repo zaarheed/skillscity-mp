f02f2678a58d44b0aa180ba28a41ff8fd3e2abc5
Zahid <zahid@zmdev.com>
2023-02-22 11:22:52 +0000

    fixed bug with image url

--- components/locations/locations-grid/location-item.js
+++ components/locations/locations-grid/location-item.js
@@ -3,7 +3,7 @@ import LikeButton from "./like-button";
 export default function LocationItem({ location }) {
     return (
         <div className="border rounded-lg p-4">
-            <img src={location.imgUrl} className="rounded-lg" />
+            <img src={location.img_url} className="rounded-lg" />
             <p className="text-xl font-medium">{location.title}</p>
             <p className="text-gray-700">{location.author}</p>
             <LikeButton />
